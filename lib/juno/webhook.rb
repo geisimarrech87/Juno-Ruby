ab1631b9308b5c6ea50e2814f9af6ab964a720cf
Geisimar Rech <geisimar.rech87@gmail.com>
2020-08-16 13:51:33 -0400

    ajuste webhiiks

--- lib/juno/webhook.rb
+++ lib/juno/webhook.rb
@@ -22,6 +22,58 @@ module Juno
 
             @response = RestClient.get base_path, header
                 
-        end        
+        end 
+        
+        def create_webhook(data)
+            header = {
+                content_type: :json,
+                "Authorization" =>  "Bearer #{token}",
+                "X-Api-Version" => Juno.api_version,
+                "X-Resource-Token" => Juno.resource_token
+            }
+    
+            begin
+                @response = RestClient.post base_path, data.to_json, header
+
+            rescue RestClient::ExceptionWithResponse => err
+                puts "erro create webhook----#{err.response}"
+                err.response
+            end  
+        end
+
+
+        def delete_webhook(id)
+            header = {
+                content_type: :json,
+                "Authorization" =>  "Bearer #{token}",
+                "X-Api-Version" => Juno.api_version,
+                "X-Resource-Token" => Juno.resource_token
+            }
+
+            begin
+                @response = RestClient.delete base_path + "/#{id}", header
+            rescue RestClient::ExceptionWithResponse => err
+                puts "erro create webhook----#{err.response}"
+                err.response
+            end  
+        end
+
+
+        def show_webhook(id)
+            header = {
+                content_type: :json,
+                "Authorization" =>  "Bearer #{token}",
+                "X-Api-Version" => Juno.api_version,
+                "X-Resource-Token" => Juno.resource_token
+            }
+    
+            begin
+                @response = RestClient.get base_path + "/#{id}", header
+            rescue RestClient::ExceptionWithResponse => err
+                puts "erro create webhook----#{err.response}"
+                err.response
+            end  
+        end
+
     end
 end
